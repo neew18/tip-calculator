ca33ba596d928245d1488d07230069b6087ef767
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-03-14 02:33:23 +0630

    Add header and bill input

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="Header text-center">
+      <p className="uppercase ">
+        Spli
+        <br />
+        tter
+      </p>
+    </div>
+  );
+}
+
+export default Header;
